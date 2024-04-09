60f1da699bd7c991327b1314335621e8ab48fda4
msLoginoff <146209985+msLoginoff@users.noreply.github.com>
2024-04-09 21:43:46 +0700

    edit Program.cs and launchSettings.json

--- MedicalInformationSystem/Program.cs
+++ MedicalInformationSystem/Program.cs
@@ -1,13 +1,23 @@
 using System.Text.Json.Serialization;
 using MedicalInformationSystem.Data;
-using MedicalInformationSystem.Jwt;
+using MedicalInformationSystem.Data.Entities;
 using MedicalInformationSystem.Requirements;
+using MedicalInformationSystem.Services;
+using MedicalInformationSystem.Services.DictionaryService;
+using MedicalInformationSystem.Services.DoctorService;
+using MedicalInformationSystem.Services.HashService;
+using MedicalInformationSystem.Services.InspectionService;
+using MedicalInformationSystem.Services.Jwt;
+using MedicalInformationSystem.Services.PatientService;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 using MvcJsonOptions = Microsoft.AspNetCore.Mvc.JsonOptions;
 using TokenHandler = MedicalInformationSystem.Requirements.TokenHandler;
 
@@ -15,7 +25,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options => 
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"), npgsqlOptions =>
+    {
+        //npgsqlOptions.MaxBatchSize(100000); // Установите желаемый размер пакета команд
+    }));
 
 builder.Services.AddAuthorization(options =>
 {
@@ -28,6 +41,7 @@ builder.Services.AddAuthorization(options =>
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
+        //options.RequireHttpsMetadata = false; //todo взято с другого проекта
         options.TokenValidationParameters = new TokenValidationParameters
         {
             // указывает, будет ли валидироваться издатель при валидации токена
@@ -48,8 +62,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
-builder.Services.AddScoped<IJwtService, JwtService>();
-builder.Services.AddScoped<IAuthorizationHandler, TokenHandler>();
+
 
 //add services for correct display enum on swagger
 builder.Services.Configure<JsonOptions>(o => o.SerializerOptions.Converters.Add(new JsonStringEnumConverter()));
@@ -68,6 +81,9 @@ builder.Services.AddSwaggerGen(swagger =>
         Title = "MIS",
         Description = "Medical Information System"
     });
+    
+    //to add annotations 
+    swagger.EnableAnnotations();
     
     //To Enable authorization using Swagger (JWT)
     swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
@@ -95,6 +111,14 @@ builder.Services.AddSwaggerGen(swagger =>
     });
 });
 
+builder.Services.AddScoped<HashPasswordService>();
+builder.Services.AddScoped<IDictionaryService, DictionaryService>();
+builder.Services.AddScoped<IDoctorService, DoctorService>();
+builder.Services.AddScoped<IPatientService, PatientService>();
+builder.Services.AddScoped<IInspectionService, InspectionService>();
+builder.Services.AddScoped<IJwtService, JwtService>();
+builder.Services.AddScoped<IAuthorizationHandler, TokenHandler>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -104,11 +128,82 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
 app.MapControllers();
+
+// 1 option
+using (var scope = app.Services.CreateScope())
+{
+     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+     db.Database.Migrate();
+     
+     if (!db.IcdRoots.Any())
+     {
+         var icdRootsJson = File.ReadAllText("IcdRoots.json");
+         var icdRoots = JsonSerializer.Deserialize<List<IcdRootsEntity>>(icdRootsJson);
+
+         foreach (var icdRoot in icdRoots)
+         {
+             icdRoot.NewId = Guid.NewGuid();
+
+             // Присваиваем старый числовой Id новому свойству OldId
+             //product.OldId = /* Значение старого числового Id */;
+
+             db.IcdRoots.Add(icdRoot);
+         }
+         db.SaveChanges();
+     }
+
+     if (!db.Icd.Any())
+     {
+         var icdJson = File.ReadAllText("ICD.json");
+         var icd = JsonSerializer.Deserialize<List<IcdEntity>>(icdJson);
+         
+         foreach (var icdItem in icd)
+         {
+             icdItem.NewId = Guid.NewGuid();
+
+             icdItem.IcdRoot = icdItem.ID switch
+             {
+                 < 933 => db.IcdRoots.First(x => x.ID == 1),
+                 < 1806 => db.IcdRoots.First(x => x.ID == 933),
+                 < 2009 => db.IcdRoots.First(x => x.ID == 1806),
+                 < 2428 => db.IcdRoots.First(x => x.ID == 2009),
+                 < 2916 => db.IcdRoots.First(x => x.ID == 2428),
+                 < 3314 => db.IcdRoots.First(x => x.ID == 2916),
+                 < 3633 => db.IcdRoots.First(x => x.ID == 3314),
+                 < 3773 => db.IcdRoots.First(x => x.ID == 3633),
+                 < 4238 => db.IcdRoots.First(x => x.ID == 3773),
+                 < 4525 => db.IcdRoots.First(x => x.ID == 4238),
+                 < 5012 => db.IcdRoots.First(x => x.ID == 4525),
+                 < 5416 => db.IcdRoots.First(x => x.ID == 5012),
+                 < 6058 => db.IcdRoots.First(x => x.ID == 5416),
+                 < 6571 => db.IcdRoots.First(x => x.ID == 6058),
+                 < 7067 => db.IcdRoots.First(x => x.ID == 6571),
+                 < 7464 => db.IcdRoots.First(x => x.ID == 7067),
+                 < 8186 => db.IcdRoots.First(x => x.ID == 7464),
+                 < 8587 => db.IcdRoots.First(x => x.ID == 8186),
+                 < 10344 => db.IcdRoots.First(x => x.ID == 8587),
+                 < 14061 => db.IcdRoots.First(x => x.ID == 10344),
+                 < 14960 => db.IcdRoots.First(x => x.ID == 14061),
+                 _ => db.IcdRoots.First(x => x.ID == 14960)
+             };
+             // Присваиваем старый числовой Id новому свойству OldId
+             //product.OldId = /* Значение старого числового Id */;
+
+             db.Icd.Add(icdItem);
+         }
+         db.SaveChanges();
+     }
+
+}
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 app.Run();
